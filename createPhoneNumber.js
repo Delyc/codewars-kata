aec99fbc3b2804d40c6dfaa96ac0499d91875b51
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2023-06-01 17:43:31 +0200

    Update createPhoneNumber.js

--- createPhoneNumber.js
+++ createPhoneNumber.js
@@ -1,5 +1,4 @@
 function createPhoneNumber(numbers){
     const phoneNumber = numbers.join('');
-    const formattedPhoneNumber = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6)}`;
-    return formattedPhoneNumber;
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6)}`;
   }
