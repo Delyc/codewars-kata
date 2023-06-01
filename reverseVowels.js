818bd94d1c5513e16934d7be5b2b195f366f85bf
Delyce Twizeyimana <d.twizeyima@alustudent.com>
2023-06-01 17:46:55 +0200

    Update reverseVowels.js

--- reverseVowels.js
+++ reverseVowels.js
@@ -1,25 +1,24 @@
 function reverseVowels(str) {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
   const strArr = str.split('');
 
   let left = 0;
   let right = strArr.length - 1;
 
   while (left < right) {
-    if (vowels.includes(strArr[left].toLowerCase()) && vowels.includes(strArr[right].toLowerCase())) {
-  
-      const temp = strArr[left];
-      strArr[left] = strArr[right];
-      strArr[right] = temp;
-      left++;
-      right--;
-    } else if (!vowels.includes(strArr[left].toLowerCase())) {
-      left++;
-    } else {
-      right--;
+    const isLeftVowel = vowels.has(strArr[left].toLowerCase());
+    const isRightVowel = vowels.has(strArr[right].toLowerCase());
+
+    const shouldSwap = isLeftVowel && isRightVowel;
+
+    if (shouldSwap) {
+      [strArr[left], strArr[right]] = [strArr[right], strArr[left]];
     }
+
+    left += Number(!isLeftVowel) || Number(shouldSwap);
+    right -= Number(!isRightVowel) || Number(shouldSwap);
   }
 
   return strArr.join('');
-  
 }
